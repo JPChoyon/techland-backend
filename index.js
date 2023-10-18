185aaeb0ea64e725faff82a5e2d4fdb62009a656
JP Choyon Khan <jpchoyonkhan65@gmail.com>
2023-10-18 14:59:59 +0600

    post and get method added for the prodact

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { connect } = require('mongo');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -12,7 +13,6 @@ app.use(cors());
 // mongo db connect
 // brand-admin
 // pmEZiotlVv4Sy5be
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.fycfdwn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +28,31 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const prodactCollection = client.db('prodactDB').collection("prodact");
+
+    app.get("/prodact", async (req, res) => {
+      const cursor = prodactCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post("/prodact", async (req, res) => {
+      const coffee = req.body;
+      const result = await prodactCollection.insertOne(coffee);
+      res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
